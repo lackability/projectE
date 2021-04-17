700a6b17a6614ce9eaf2a4d208c7fdb0ad04a9a1
Lackability <5019arjin@gmail.com>
2021-04-16 20:35:50 -0500

    reversing int

--- project4.cpp
+++ project4.cpp
@@ -2,20 +2,24 @@
 // A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
+#include <algorithm> 
 #include <iostream>
 #include <vector>
-#include <cmath>
+
 
 int pMultiples(){
     for (int number1 = 1; number1 <= 99; number1++){
+        int temp = 0;
         for (int number2 = 1; number2 <= 99; number2++){
-            if (number1 * number2)
-        }
+            temp = number1*number2;
+            if (reverse(temp)){
+            }
+        }   return temp;
     }
 }
 
 
 
 int main(){
-
+    std::cout << pMultiples() << std::endl;
 }
---
+++ reverse.cpp
@@ -0,0 +1,13 @@
+
+
+#include <iostream>
+#include <vector>
+
+int reverseNum(int i){
+    for 
+
+}
+
+int main(){
+    std::cout << reverseNum(500) << std::endl;
+}

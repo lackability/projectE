fd077246f66cc159df18ba4b0c80d048a8b49b21
Lackability <5019arjin@gmail.com>
2021-04-16 20:10:43 -0500

    hello

--- project4.cpp
+++ project4.cpp
@@ -1,1 +1,21 @@
 //project 4
+// A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+// Find the largest palindrome made from the product of two 3-digit numbers.
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+int pMultiples(){
+    for (int number1 = 1; number1 <= 99; number1++){
+        for (int number2 = 1; number2 <= 99; number2++){
+            if (number1 * number2)
+        }
+    }
+}
+
+
+
+int main(){
+
+}
